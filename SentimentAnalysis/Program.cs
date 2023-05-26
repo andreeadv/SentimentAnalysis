79d48bdace3daab6e3c27178b1ced4659cd1388c
andreeadv <andreeavioletadinu@gmail.com>
2023-05-26 20:52:19 +0300

    Add project files.

---
+++ SentimentAnalysis/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using SentimentAnalysis;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
+namespace SentimentAnalysis
+{
+    internal class Program
+    {
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+            TrainTestData splitDataView = LoadData(mlContext);
+            ITransformer model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+            Evaluate(mlContext, model, splitDataView.TestSet);
+            UseModelWithSingleItem(mlContext, model);
+            UseModelWithBatchItems(mlContext, model);
+        }
+        public static TrainTestData LoadData(MLContext mlContext)
+        {
+            // Note that this case, loading your training data from a file,
+            // is the easiest way to get started, but ML.NET also allows you
+            // to load data from databases or in-memory collections.
+            // <SnippetLoadData>
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            // </SnippetLoadData>
+
+            // You need both a training dataset to train the model and a test dataset to evaluate the model.
+            // Split the loaded dataset into train and test datasets
+            // Specify test dataset percentage with the `testFraction`parameter
+            // <SnippetSplitData>
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+            // </SnippetSplitData>
+
+            // <SnippetReturnSplitData>
+            return splitDataView;
+            // </SnippetReturnSplitData>
+        }
+        public static ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+            return model;
+        }
+        public static void Evaluate(MLContext mlContext, ITransformer model, IDataView splitTestSet)
+        {
+            Console.WriteLine("=============== Evaluating Model accuracy with Test data===============");
+            IDataView predictions = model.Transform(splitTestSet);
+            CalibratedBinaryClassificationMetrics metrics = mlContext.BinaryClassification.Evaluate(predictions, "Label");
+            Console.WriteLine();
+            Console.WriteLine("Model quality metrics evaluation");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.AreaUnderRocCurve:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+            Console.WriteLine("=============== End of model evaluation ===============");
+        }
+        public static void UseModelWithSingleItem(MLContext mlContext, ITransformer model)
+        {
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+            SentimentData sampleStatement = new SentimentData
+            {
+                SentimentText = "Spend your money and time some place else."
+            };
+            var resultPrediction = predictionFunction.Predict(sampleStatement);
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+
+        }
+        public static void UseModelWithBatchItems(MLContext mlContext, ITransformer model)
+        {
+            IEnumerable<SentimentData> sentiments = new[]
+{
+            new SentimentData
+            {
+                SentimentText = "I can't tell you how disappointed I was."
+            },
+            new SentimentData
+            {
+            SentimentText = "Mi esposo dijo que ella fue muy grosera... ni siquiera se disculpó por la mala comida ni nada."
+            }
+            };
+            IDataView batchComments = mlContext.Data.LoadFromEnumerable(sentiments);
+
+            IDataView predictions = model.Transform(batchComments);
+
+            // Use model to predict whether comment data is Positive (1) or Negative (0).
+            IEnumerable<SentimentPrediction> predictedResults = mlContext.Data.CreateEnumerable<SentimentPrediction>(predictions, reuseRowObject: false);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of loaded model with multiple samples ===============");
+            
+            foreach (SentimentPrediction prediction in predictedResults)
+            {
+                Console.WriteLine($"Sentiment: {prediction.SentimentText} | Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Spanish" : "English")} | Probability: {prediction.Probability} ");
+            }
+            Console.WriteLine("=============== End of predictions ===============");
+
+
+        }
+    }
+    
+}
